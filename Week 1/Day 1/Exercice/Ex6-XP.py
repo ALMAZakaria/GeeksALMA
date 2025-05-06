90c53e626e7ed9e4ee42989602b972f86119474f
ALMA Zakaria <zakariaalma.business@gmail.com>
2025-05-06 11:43:01 +0100

    Replace Gold and Ninja Exercice to folders with there names

--- Week 1/Day 1/Exercice/Ex6-XP.py
+++ Week 1/Day 1/Exercice/Ex6-XP.py
@@ -4,4 +4,4 @@ print("Original tuple:", integers_tuple)
 print("Is it possible to add more integers to the tuple?")
 print("No, tuples are immutable. We would need to create a new tuple.")
 new_tuple = integers_tuple + (5, 6)
-print("New tuple created by concatenation:", new_tuple
+print("New tuple created by concatenation:", new_tuple)
--- Week 1/Day 1/index.py
+++ Week 1/Day 1/index.py
@@ -1,1 +1,18 @@
-print("hiii")
+#Create a set called my_fav_numbers with all your favorites numbers.
+my_fav_n = {0,2,5,1,10}
+print("My favorite numbers initially:", my_fav_n)
+
+#Add two new numbers to the set.
+my_fav_n.add(10)
+my_fav_n.add(11)
+print("My favorite numbers initially:", my_fav_n)
+
+
+fav_list = list(my_fav_n)
+fav_list.pop()
+
+
+friend_fav_n = {0,9,11,3,6}
+print("My favorite numbers initially:", my_fav_n)
+
+
