454ac54d35ee0d6fe2a3b40feffeb31de7b155c6
ALMA Zakaria <zakariaalma.business@gmail.com>
2025-05-22 11:58:34 +0100

    day 3

---
+++ Week 3/Day 2/arrays.js
@@ -0,0 +1,15 @@
+let pets = ["cat", "dog", "fish", "rabbit", "cow"];
+// Display dog
+console.log(pets[1]);
+//  Add to the array pets, the pet horse.
+pets.push("horse");
+console.log(pets)
+
+//Remove the pet rabbit
+pets.splice(pets.indexOf('rabbit', 1));
+console.log(pets);
+
+//4. Display the array length
+
+
+
